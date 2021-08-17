d9f63fa7060cff1a345f3829cd39601c1661398c
BananaKun <yanxun951224@gmail.com>
2021-08-17 15:23:03 +0100

    Ex1 - Ex3

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
 window.onload = () => {
-    getUser();
+
+    displayUser();
+    displayUsername();
 }
 
-const getUser = async () => {
-    let response = await fetch("https://jsonplaceholder.typicode.com/users");
-    let data = await response.json();
-    console.log(data)
-    // .then((response) => response.json())
-    // .then((result) => {
-    //     console.log(result);
-    // })
-    // .catch(err => {
-    //     console.log("rejected");
-    //     console.error(err);
-    // });
-    // console.log("test")
+const rowUser = document.querySelector(".row.all-user");
+const rowUsername = document.querySelector(".row.all-user-name");
+let userArr = [];
+
+const displayUser = async (filter = "https://jsonplaceholder.typicode.com/users") => {
+    let response = await fetch(filter);
+    let users = await response.json();
+    userArr = users;
+    rowUser.innerHTML = ""
+
+    userArr.forEach(user => {
+        rowUser.innerHTML += `<div class="col-sm">
+          <div class="card" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title">${user.name}</h5>
+              <p class="card-text">
+                ${user.email}
+              </p>
+            </div>
+          </div>
+        </div>`
+    });
 }
+
+const displayUsername = async (filter = "https://jsonplaceholder.typicode.com/users") => {
+    let response = await fetch(filter);
+    let users = await response.json();
+
+    users.forEach(user => {
+        rowUsername.innerHTML += `<div class="col-sm">
+          <div class="card" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title">${user.name}</h5>
+              </p>
+            </div>
+          </div>
+        </div>`
+    });
+}
+
+const getUserInput = (e) => {
+    let query = e.target.value;
+    // let query = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+    if (query.length > 3) {
+        let searchFilter = document.querySelector(".custom-select").value;
+        filterUser(query, searchFilter);
+    } else if (query.length == 0) {
+        filterUser();
+    }
+}
+
+// const capitalizeFirstLetter = (str) => {
+
+// }
+
+const filterUser = (query, filter) => {
+    filter == "name" ? displayUser(`https://jsonplaceholder.typicode.com/users?name=${query}`)
+        : filter == "username" ? displayUser(`https://jsonplaceholder.typicode.com/users?username=${query}`)
+            : filter == "email" ? displayUser(`https://jsonplaceholder.typicode.com/users?email=${query}`)
+                : displayUser();
+}
+
